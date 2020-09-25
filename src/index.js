38108ad954628f4e8638cdcbbe9fd7bb30207ba6
dwandrew <danwandrew@gmail.com>
2020-09-25 10:00:23 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+function changeToBlue(event){
+  event.target.style.color = "blue"
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={changeToBlue} />
   </div>,
   document.getElementById('root')
 );
