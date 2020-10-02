c3cb8fa778d384f1c4eb8c819ad79339bda47be2
mspcity <codepark2019@gmail.com>
2020-10-02 02:39:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function changeToPink(event){
+  event.target.style.color = "pink"
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={changeToPink} />
   </div>,
   document.getElementById('root')
 );
