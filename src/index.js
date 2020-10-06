74b4d35843d85c991d7b1ecd985c2ccca3488a81
ejguzman3988 <eriberto.guzman@outlook.com>
2020-10-06 00:05:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = (event) => {
+  event.target.style.color === 'blue' ? event.target.style.color = 'red' : event.target.style.color = 'blue'
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
